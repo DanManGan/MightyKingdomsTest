d4f6fe26ead4e2c356ab30946cb7f009c2802db0
Daniel Manganaro <manganaro.daniel@gmail.com>
2019-01-23 09:57:11 +0800

    Redid player controller
    Redid the player controller adding functions for moving, shooting, turning and animating. Added gun effects

--- Assets/_Game/Scripts/Effects/Destructible.cs
+++ Assets/_Game/Scripts/Effects/Destructible.cs
@@ -6,10 +6,15 @@ public class Destructible : MonoBehaviour
 {
     public GameObject destroyedVersion;
     public float health = 100f;
+    public ParticleSystem hitParticles;
 
-    public void TakeDamage(float damage)
+
+
+    public void TakeDamage(int damage, Vector3 hitPoint)
     {
         health = health - damage;
+        hitParticles.transform.position = hitPoint;
+        hitParticles.Play();
 
         if (health <= 0) {
             Instantiate(destroyedVersion, transform.position, transform.rotation);
--- Assets/_Game/Scripts/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -7,20 +7,24 @@ public class PlayerController : MonoBehaviour
     public Joystick leftJoystick;               // Reference to the left joystick
     public Joystick rightJoystick;              // Reference to the right joystick
     public Transform gunpoint;                  // The position of the gunpoint for ray tracing and muzzle flash particle effect
+    public bool isShooting = false;             // Bool to see if the player is currently shooting or not
     public float runSpeed = 6f;                 // The speed that the player will move at.
     public float firingRange = 50f;             // The range of which the player can shoot
-    public float damage = 10f;                  // The damage for each shot
+    public int damage = 20;                     // The damage for each shot
+    public float timeBetweenBullets = 0.15f;    // The time between each shot
+    public ParticleSystem m_GunParticles;      // Reference to the particle system
+    public LineRenderer m_GunLine;             // Reference to the line renderer
+    public AudioSource m_GunAudio;             // Reference to the audio source
+    public Light m_GunLight;                   // Reference to the light component
 
+    private float m_Timer;                      // A timer to detmine when to fire
+    private Ray m_ShootRay;                     // A ray from the gun end forwards
+    private RaycastHit m_ShootHit;              // A raycast hit to get information about what was hit
+    private float m_EffectsDisplayTime = 0.2f;  // The proportion of the timeBetweenBullets that the effect will display for
     private Rigidbody m_Rigidbody;              // Reference to the player's rigidbody
     private Vector3 m_Movement;                 // The vector to store the direction of the player's movement
     private Vector3 m_ShootDir;                 // The vector to store the direction of the player's shooting
-    private Vector3 m_CurrLookDir;              // The vector to store the current direction of the player
-    private Vector3 m_NewLookDir;               // The vector to store the new direction of the player 
     private Animator m_Anim;                    // Reference to the animator component
-    private float m_HorizontalMove;             // Float to store the horizontal movement input
-    private float m_VerticalMove;               // Float to store the vertical movement input
-    private float m_HorizontalShoot;            // Float to store the horizontal shooting input
-    private float m_VerticalShoot;              // Float to store the vertical shooting input
 
     // Start is called before the first frame update
     void Start()
@@ -28,70 +32,115 @@ public class PlayerController : MonoBehaviour
         // Set up references
         m_Rigidbody = GetComponent<Rigidbody>();
         m_Anim = GetComponent<Animator>();
+        m_GunParticles.Stop();
+        m_GunLight.enabled = false;
+        m_GunLine.enabled = false;
     }
 
     private void Update()
     {
-        // Store the input axes for movement and shooting
-        m_HorizontalMove = leftJoystick.Horizontal;
-        m_VerticalMove = leftJoystick.Vertical;
+        // Add the time since Update was last called to the timer
+        m_Timer += Time.deltaTime;        
 
-        m_HorizontalShoot = rightJoystick.Horizontal;
-        m_VerticalShoot = rightJoystick.Vertical;
+        // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for
+        if(m_Timer >= timeBetweenBullets * m_EffectsDisplayTime) {
+            DisableEffects();
+        }
     }
 
     private void FixedUpdate()
     {
+        // Store the input axes for movement and shooting
+        float hMove = leftJoystick.Horizontal;
+        float vMove = leftJoystick.Vertical;
+
+        float hShoot = rightJoystick.Horizontal;
+        float vShoot = rightJoystick.Vertical;
+
+        if (hMove != 0 || vMove != 0) {
+            // Move the player
+            Move(hMove, vMove);
+        }
+                
+        // Set isShooting if the player is shooting
+        if (hShoot != 0 || vShoot != 0) {
+            isShooting = true;
+
+            if (m_Timer >= timeBetweenBullets) {
+                // Shoot the gun
+                Shoot(hShoot, vShoot);
+            }
+        } else {
+            isShooting = false;
+        }        
+
+        // Rotate the player
+        Turning();
+
+        // Animate the player.
+        Animating(hMove, vMove, hShoot, vShoot);
+    }
+
+    public void DisableEffects()
+    {
+        m_GunLine.enabled = false;
+        m_GunLight.enabled = false;
+    }
+
+    void Move(float h, float v)
+    {
         // Set the movement vector based on the left joystick input
-        m_Movement.Set(m_HorizontalMove, 0f, m_VerticalMove);
-
-        // Set the shooting vector based on the right joystick input
-        m_ShootDir.Set(m_HorizontalShoot, 0f, m_VerticalShoot);
-
-        // Move the player to new rotation
-        if (m_HorizontalShoot == 0 && m_VerticalShoot == 0) {
-            m_NewLookDir.Set(m_HorizontalMove, 0f, m_VerticalMove);
-
-            if (m_NewLookDir != m_CurrLookDir) {
-                m_CurrLookDir = m_NewLookDir;
-                m_Rigidbody.MoveRotation(Quaternion.LookRotation(m_CurrLookDir.normalized, Vector3.up));
-            }
-        }
-
-        if (m_HorizontalShoot != 0 || m_VerticalShoot != 0) {
-            Shoot(m_HorizontalShoot, m_VerticalShoot);
-
-            m_NewLookDir.Set(m_HorizontalShoot, 0f, m_VerticalShoot);
-
-            if (m_NewLookDir != m_CurrLookDir) {
-                m_CurrLookDir = m_NewLookDir;
-                m_Rigidbody.MoveRotation(Quaternion.LookRotation(m_CurrLookDir.normalized, Vector3.up));
-            }
-        }
+        m_Movement.Set(h, 0f, v);
 
         // Normalize the movement vector and make it proportional to the speed per second
         m_Movement = m_Movement.normalized * runSpeed * Time.deltaTime;
 
         // Move the player to it's current position plus the movement
         m_Rigidbody.MovePosition(transform.position + m_Movement);
-
-        //Animate the player.
-        Animating(m_HorizontalMove, m_VerticalMove, m_HorizontalShoot, m_VerticalShoot);
-
-        if(m_HorizontalShoot != 0 || m_VerticalShoot != 0) {
-            m_Anim.SetBool("IsFiring", true);
-        } else {
-            m_Anim.SetBool("IsFiring", false);
-        }
     }
 
     void Shoot(float h, float v)
     {
-        RaycastHit hitInfo;
+        // Set the shooting vector based on the right joystick input
+        m_ShootDir.Set(h, 0f, v);
 
-        if (Physics.Raycast(gunpoint.position, gunpoint.forward, out hitInfo, firingRange)) {
-            Debug.Log(hitInfo.transform.name);
-            Debug.DrawLine(gunpoint.position, hitInfo.transform.position);
+        // Reset the timer
+        m_Timer = 0f;
+
+        // Play the gun shot audioclip
+        m_GunAudio.Play();
+
+        // Enable the light
+        m_GunLight.enabled = true;
+
+        // Stop the partibles from playing if they were, then start the particles
+        m_GunParticles.Stop();
+        m_GunParticles.Play();
+
+        // Enable the line renderer and set it's first position to the end of the gun
+        m_GunLine.enabled = true;
+        m_GunLine.SetPosition(0, transform.position);
+
+        // Set the shoot ray so that it starts at the end of the fun and points forward from the barrel
+        m_ShootRay.origin = transform.position;
+        m_ShootRay.direction = transform.forward;
+                
+        // Perform the raycast
+        if (Physics.Raycast(m_ShootRay, out m_ShootHit, firingRange)) {
+            // Hit something, deal damage
+            Debug.Log(m_ShootHit.transform.name);
+            Debug.DrawLine(gunpoint.position, m_ShootHit.transform.position);
+        } else {
+            m_GunLine.SetPosition(1, m_ShootRay.origin + m_ShootRay.direction * firingRange);
+        }
+    }
+
+    void Turning()
+    {
+        if (isShooting) {
+            m_Rigidbody.MoveRotation(Quaternion.LookRotation(m_ShootDir.normalized, Vector3.up));
+        } else {
+            m_Rigidbody.MoveRotation(Quaternion.LookRotation(m_Movement.normalized, Vector3.up));
         }
     }
 
